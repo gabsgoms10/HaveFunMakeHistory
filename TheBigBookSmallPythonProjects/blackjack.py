5339e0e141a71e859ded5409ac25d3fd2ec62884
Gabriel Cipriano <84808116+gabsgoms10@users.noreply.github.com>
2023-11-09 13:14:09 -0300

    Started Main loop

--- TheBigBookSmallPythonProjects/blackjack.py
+++ TheBigBookSmallPythonProjects/blackjack.py
@@ -5,6 +5,23 @@ DIAMONDS = chr(9830)
 SPADES = chr(9824)
 CLUBS = chr(9827)
 
+def main():
+    print('''BlackJack Rules:
+    Try to get as close to 21 without going over.
+    Kings, Queens and Jacks are worth 10 points.
+    Aces are worth 1 or 11 points.
+    Cards 2 through 10 are worth their face value.
+    (H)it to take another card.
+    (S)tand to stop taking cards.
+    On your first play, you can (D)ouble down to increase your bet
+    but must hit exactly one more time before standing.
+    In case of a tie, the bet is returned to the player.
+    The dealer stops hitting at 17.''')
+
+    money = 5000
+
+    while True: #Main loop
+
 
 def getBet(maxBet):
     # We'll ask the player how much money the bet will be
@@ -18,7 +35,17 @@ def getBet(maxBet):
             continue #For when the input is not decimal but is not "QUIT"
         bet = int(bet)
         if 1<=bet <=maxBet:
-            return bet
+            return bet #Player entered a valid bet
+
+def getDeck():
+    deck =[]
+    for suit in (HEARTS, DIAMONDS, SPADES, CLUBS):
+        for rank in range(2,11):
+            deck.append((str(rank), suit))
+        for rank in ("J", "Q", "K", "A"):
+            deck.append((rank, suit))
+    random.shuffle(deck)
+    return deck
 
 
 
